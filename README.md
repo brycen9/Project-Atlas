9281f9fd53a84026222bfba995663dbf9073ccc0
Bryson <brycenbonawitt9@gmail.com>
2026-09-10 21:38:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,83 @@
+# Project Atlas 🚀
+
+Engineering software for asteroid exploration and mission planning.
+
+---
+
+## Vision
+
+Project Atlas is a long-term software project focused on helping engineers and researchers explore, analyze, and eventually plan missions to near-Earth asteroids.
+
+The goal is to build this project over the next several years while studying Intelligent Systems Engineering at Indiana University.
+
+---
+
+## Version
+
+Current Version: **0.1.0**
+
+Status: 🟢 In Development
+
+---
+
+## Features (Planned)
+
+- Search real asteroids
+- Display asteroid information
+- Visualize asteroid orbits
+- Estimate mission difficulty
+- Plan future missions
+- AI-assisted mission recommendations
+
+---
+
+## Roadmap
+
+### Version 0.1
+- [ ] Project setup
+- [ ] Search asteroid
+- [ ] Display asteroid information
+
+### Version 0.2
+- [ ] Orbit visualization
+- [ ] Interactive interface
+
+### Version 0.3
+- [ ] Mission planning
+
+### Version 0.4
+- [ ] Trajectory calculations
+
+### Version 0.5
+- [ ] AI-assisted recommendations
+
+---
+
+## Tech Stack
+
+- Python
+- Git
+- GitHub
+
+Future:
+- Streamlit
+- Plotly
+- NASA APIs
+- Machine Learning
+
+---
+
+## Why I'm Building This
+
+I'm building Project Atlas as a long-term engineering project to improve my software engineering skills while exploring autonomous systems and future space technologies.
+
+This project will evolve throughout my college career and serve as a public record of my growth as an engineer.
+
+---
+
+## Author
+
+Brycen Bonawitt
+
+Intelligent Systems Engineering  
+Indiana University
---
+++ app.py
@@ -0,0 +1,85 @@
+import requests
+
+def main():
+    greeting()
+
+def greeting():
+    text = "Project Atlas"
+    centered_text = text.center(40)
+    print("=" * 40)
+    print(centered_text)
+    print("=" * 40)
+    print()
+    print("Welcome, Commander.")
+    print()
+    print("Current Version: 0.1")
+    print()
+    print("-" * 40)
+    print()
+    print("1. Search Asteroid")
+    print()
+    print("2. Exit")
+    print()
+    print("-" * 40)
+    print()
+
+    user_input = 0
+    while user_input != 1 and user_input != 2:
+        try:
+            user_input = int(input("Enter your selection: "))
+        except ValueError:
+            print("Must enter 1 or 2.")
+            print()
+            continue
+
+        if (user_input == 1):
+            print()
+            search_asteroid()
+            return
+            
+        elif (user_input == 2):
+            print("Shutting down...Goodbye")
+            return
+
+        else:
+            print()
+            print("Error! Please enter 1 or 2.")
+            print()
+
+        
+
+
+def search_asteroid():
+    text = "Asteroid Database"
+    centered_text = text.center(27)
+    print("=" * 27)
+    print(centered_text)
+    print("=" * 27)
+    print()
+
+    asteroid_name = input("Enter the asteroid name: ")
+    print()
+    print(f"Searching for {asteroid_name}...")
+
+    url = f"https://ssd-api.jpl.nasa.gov/sbdb.api?sstr={asteroid_name}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+    text = "ASTEROID FOUND"
+    centered_text = text.center(28)
+    print("=" * 28)
+    print(centered_text)
+    print("=" * 28)
+    print()
+    print(data)
+    print(f"Name: {data["object"]["shortname"]}")
+    print(f"Designation: {data["object"]["des"]}")
+    print(f"Near-Earth Object: {data["object"]["neo"]}")
+    print(f"Potentially Hazardous: {data["object"]["pha"]}")
+    print()
+
+    
+if __name__ == "__main__":
+    main()
